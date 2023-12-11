bfe5b810c1fa1124084a0226e02982cc74e76dd7
KarenHakobyan <95857042+Karen-Hakobya-n@users.noreply.github.com>
2023-12-11 17:13:54 +0400

    Add files via upload

---
+++ coin_counter.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+model = YOLO('C:/Users/User/Downloads/best (1).pt')
+results = model.predict(source='Test_images/20231211_164424.jpg', save=False, imgsz=(640, 640), conf=0.7)
+boxes = []
+path = str()
+for result in results:
+    boxes = list(result.boxes.cls)  # Boxes object for bbox cls outputs
+    path = ''.join(str(result.path))
+
+
+coins = {0: 10, 1: 20, 2: 50, 3: 100, 4: 200, 5: 500}
+
+amount_coins = []
+
+for i in boxes:
+    amount_coins.append(coins[int(i)])
+
+print(f'Image Path is {path}')
+print(f'{sum(amount_coins)} AMD')
+
